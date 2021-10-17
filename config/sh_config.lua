8009f68c817d931b731ad679151dfeb55a86a241
clementinise <35346472+clementinise@users.noreply.github.com>
2021-10-17 17:58:57 +0200

    Modify Config to use it as an example in client

--- config/sh_config.lua
+++ config/sh_config.lua
@@ -1,4 +1,4 @@
 -- Wowow an epic shared file
 Config = {} -- We must initialise the config table to be able to do config.anything
 
-Config.printText = "Hello world!" -- The text to be printed
+Config.WelcomeText = "Welcome to the server !" -- The text to be printed
